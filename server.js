b76de7030d0d0cf639c9b6b2dac137fe1e990038
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-24 20:48:36 -0400

    add inquirer prompts

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log('hello world!');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+const cTable = require('console.table');
+const department = require('./db/schema.sql');
+
+
+
+function questions(){
+    console.log("     |   |  | |||    EMPLOYEE TRACKER    ||| |  |  |    ")
+    inquirer
+    .prompt([
+    {
+      type: 'list',
+      name: 'see',
+      message: 'What would you like to do?',
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+      //validate: userInput => {
+      //  if (userInput) {
+       //   return console.table(department);
+       // } else if (userInput === 'view all roles'){
+       //   return console.table(role);
+      //  }
+     }])
+    };
